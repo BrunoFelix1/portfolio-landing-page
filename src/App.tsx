d084175f8d0898731180a51db89c72de73945d77
Bruno Felix <brunofelixpinto@gmail.com>
2026-04-29 11:03:22 -0300

    chore: adding shadcn and tailwind v4

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,6 @@ function App() {
       <section className="bg-gray-800 p-8 rounded-lg text-center">
         <h1>Portfólio Saúde</h1>
         <p>Bem-vindo ao seu novo projeto React com Vite e TypeScript.</p>
-        <a>Começar agora</a>
       </section>
     </main>
   );
